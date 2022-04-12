1686ffa48ea9f1ee5e4f2e064620f7678f2b0e37
Jenny <JMcDaniel36@hotmail.com>
2022-04-12 17:20:14 -0600

    Updates

--- capstone/src/main/java/com/techelevator/view/Menu.java
+++ capstone/src/main/java/com/techelevator/view/Menu.java
@@ -1,8 +1,12 @@
 package com.techelevator.view;
 
+//import com.techelevator.TypeOfItem;
+import com.techelevator.VendingMachineItem;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Menu {
@@ -13,6 +17,26 @@ public class Menu {
 	public Menu(InputStream input, OutputStream output) {
 		this.out = new PrintWriter(output);
 		this.in = new Scanner(input);
+	}
+
+	public Menu() {
+		HashMap<String, VendingMachineItem> vendingMachineItems = new HashMap<String, VendingMachineItem>(){};
+		vendingMachineItems.put("A1", new VendingMachineItem("Potato Crisps", 3.05, "Chip", 5));
+		vendingMachineItems.put("A2", new VendingMachineItem("Stackers",1.45, "Chip", 5));
+		vendingMachineItems.put("A3", new VendingMachineItem("Grain Waves", 2.75, "Chip", 5));
+		vendingMachineItems.put("A4", new VendingMachineItem("Cloud Popcorn", 3.65, "Chip", 5));
+		vendingMachineItems.put("B1", new VendingMachineItem("Moonpie", 1.80, "Candy", 5));
+		vendingMachineItems.put("B2", new VendingMachineItem("Cowtales", 1.50 , "Candy", 5));
+		vendingMachineItems.put("B3", new VendingMachineItem("Wonka Bar", 1.50,"Candy",5));
+		vendingMachineItems.put("B4", new VendingMachineItem("Crunchie", 1.75,"Candy",5));
+		vendingMachineItems.put("C1", new VendingMachineItem("Cola", 1.25,"Drink",5));
+		vendingMachineItems.put("C2", new VendingMachineItem("Dr. Salt", 1.50,"Drink",5));
+		vendingMachineItems.put("C3", new VendingMachineItem("Mountain Melter", 1.50,"Drink",5));
+		vendingMachineItems.put("C4", new VendingMachineItem("Heavy", 1.50,"Drink",5));
+		vendingMachineItems.put("D1", new VendingMachineItem("U-Chews", .85,"Gum",5));
+		vendingMachineItems.put("D2", new VendingMachineItem("Little League Chew", .95,"Gum",5));
+		vendingMachineItems.put("D3", new VendingMachineItem("Chiclets", .75,"Gum",5));
+		vendingMachineItems.put("D4", new VendingMachineItem("Triplemint", .75,"Gum",5));
 	}
 
 	public Object getChoiceFromOptions(Object[] options) {
